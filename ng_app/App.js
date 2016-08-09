55a5dacabcb68f3cd8e0e099e3363a28edd33285
chin2km <me.chintukm@gmail.com>
2016-08-09 23:30:31 +0530

    Add

--- ng_app/App.js
+++ ng_app/App.js
@@ -14,7 +14,7 @@ var chin2km = angular.module("chin2km", [
 
 chin2km.run(function ($route,$rootScope, $location, AppService) {
 
-    AppService.LoadTimer(8000,"I'm undefined (for now!)")
+    //AppService.LoadTimer(8000,"I'm undefined (for now!)")
 
 })
 
