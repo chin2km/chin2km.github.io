5f47bd3eb02df5ff757ba243cd18341dac27b61b
Chintu Kishen KM <me.chintukm@gmail.com>
2016-08-10 01:02:23 +0530

    add

--- ng_app/App.js
+++ ng_app/App.js
@@ -13,8 +13,6 @@ var chin2km = angular.module("chin2km", [
 
 chin2km.run(function ($route,$rootScope, $location, AppService) {
 
-    //AppService.LoadTimer(8000,"I'm undefined (for now!)")
-
 })
 
 
